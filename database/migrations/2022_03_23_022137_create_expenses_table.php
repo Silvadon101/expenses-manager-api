d22dc5f88ddef339017c77e8bb46e9fc4507da21
Don_Silva <83092440+Silvadon101@users.noreply.github.com>
2022-03-22 21:39:46 -0500

    Added Expense Table in DB

--- database/migrations/2022_03_23_022137_create_expenses_table.php
+++ database/migrations/2022_03_23_022137_create_expenses_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('expenses', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
